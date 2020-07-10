1886ebe8e51210cc5f57d0e11ae276c6ecf02323
Aladin Taleb <aladin.taleb@gmail.com>
2020-07-11 01:05:03 +0200

    [feature] save data

--- src/pages/Contact.tsx
+++ src/pages/Contact.tsx
@@ -1,13 +1,18 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, SyntheticEvent } from "react";
 import { useParams } from "react-router-dom";
 import API from "../service/api";
-import { Modal } from "antd";
+import { Modal, Input, Button } from "antd";
+import delay from "../utils/delay";
 
 const Contact = () => {
   const { contactId } = useParams();
 
-  const [isFetching, setIsFetching] = useState<Boolean>(false);
+  const [isFetching, setIsFetching] = useState<boolean>(false);
+  const [isUpdating, setIsUpdating] = useState<boolean>(false);
+
   const [contact, setContact] = useState(null);
+
+  const [contactName, setContactName] = useState("");
 
   useEffect(() => {
     if (contactId) {
@@ -15,11 +20,16 @@ const Contact = () => {
     }
   }, [contactId]);
 
+  ///////////////////////////////////////////
+  // Network
+  ///////////////////////////////////////////
   const fetch = async (contactId: string) => {
     setIsFetching(true);
     try {
       const response = await API.contact.getContact(contactId);
-      setContact(response.data.contact);
+      const { contact } = response.data;
+      setContact(contact);
+      setContactName(contact.name);
     } catch (e) {
       //TODO: Error
       Modal.error({
@@ -30,9 +40,61 @@ const Contact = () => {
     }
   };
 
+  const update = async (contactId: string, contactInfos: any) => {
+    setIsUpdating(true);
+    //TODO: Check values
+
+    await delay(2000);
+    try {
+      const response = await API.contact.updateContact(contactId, contactInfos);
+      console.log(response);
+    } catch (e) {
+      //TODO: Error
+    } finally {
+      setIsUpdating(false);
+    }
+  };
+
+  ///////////////////////////////////////////
+  // Name Cb
+  ///////////////////////////////////////////
+
+  const handleContactNameChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setContactName(e.target.value);
+  };
+
+  ///////////////////////////////////////////
+  // Buttons Cb
+  ///////////////////////////////////////////
+
+  const handleSaveClick = () => {
+    const contactInfos = {
+      id: contactId,
+      name: contactName,
+    };
+
+    update(contactId, contactInfos);
+  };
+
+  ///////////////////////////////////////////
+  // Render
+  ///////////////////////////////////////////
   return (
     <div data-testid="contact-page">
-      Contact<div>{JSON.stringify(contact)}</div>
+      <div>
+        <div>Name</div>
+        <Input value={contactName} onChange={handleContactNameChange} />
+      </div>
+      <div>
+        <Button
+          type="primary"
+          disabled={isFetching || isUpdating}
+          loading={isUpdating}
+          onClick={handleSaveClick}
+        >
+          Save
+        </Button>
+      </div>
     </div>
   );
 };
