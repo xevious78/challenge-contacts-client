71ce661ef908c8a1c3c50a15452ae78e65114a4c
Aladin Taleb <aladin.taleb@gmail.com>
2020-07-11 00:45:28 +0200

    [feature] fetch given contact in contact page

--- src/pages/Contact.tsx
+++ src/pages/Contact.tsx
@@ -1,7 +1,40 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
+import API from "../service/api";
+import { Modal } from "antd";
 
 const Contact = () => {
-  return <div data-testid="contact-page">Contact</div>;
+  const { contactId } = useParams();
+
+  const [isFetching, setIsFetching] = useState<Boolean>(false);
+  const [contact, setContact] = useState(null);
+
+  useEffect(() => {
+    if (contactId) {
+      fetch(contactId);
+    }
+  }, [contactId]);
+
+  const fetch = async (contactId: string) => {
+    setIsFetching(true);
+    try {
+      const response = await API.contact.getContact(contactId);
+      setContact(response.data.contact);
+    } catch (e) {
+      //TODO: Error
+      Modal.error({
+        title: "Error",
+      });
+    } finally {
+      setIsFetching(false);
+    }
+  };
+
+  return (
+    <div data-testid="contact-page">
+      Contact<div>{JSON.stringify(contact)}</div>
+    </div>
+  );
 };
 
 export default Contact;
