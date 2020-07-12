50d30374c095c9ebcb0646cf43fbc6074eea97b7
Aladin Taleb <aladin.taleb@gmail.com>
2020-07-12 16:33:35 +0200

    [feature] delete contacts from contact list

--- src/components/Contact/ContactCell.tsx
+++ src/components/Contact/ContactCell.tsx
@@ -1,26 +1,55 @@
 import React from "react";
 import { Contact } from "../../types";
+import { Button } from "antd";
 
 type Props = {
   contact: Contact;
+  deleteDisabled?: boolean;
+  isDeleting?: boolean;
   onClick: (contact: Contact) => void;
+  onDeleteClick: (contact: Contact) => void;
 };
 
 const ContactCell = React.memo<Props>((props) => {
-  const { contact, onClick } = props;
+  const { contact, isDeleting = false, deleteDisabled = false } = props;
+  const { onClick, onDeleteClick } = props;
 
   ///////////////////////////////////////////
   // Cb
   ///////////////////////////////////////////
 
   const handleClick = () => {
+    if (isDeleting) {
+      return;
+    }
     onClick(contact);
+  };
+
+  const handleDeleteClick = (e: React.MouseEvent) => {
+    if (isDeleting) {
+      return;
+    }
+
+    e.preventDefault();
+    e.stopPropagation();
+    onDeleteClick(contact);
   };
 
   ///////////////////////////////////////////
   // Render
   ///////////////////////////////////////////
-  return <div onClick={handleClick}>{contact.name}</div>;
+  return (
+    <div onClick={handleClick}>
+      {contact.name}
+      <Button
+        loading={isDeleting}
+        disabled={isDeleting || deleteDisabled}
+        onClick={handleDeleteClick}
+      >
+        Delete
+      </Button>
+    </div>
+  );
 });
 
 export default ContactCell;
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,14 +1,39 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { observer, useObserver } from "mobx-react";
 import { useStores } from "../stores";
 import ContactCell from "../components/Contact/ContactCell";
 import { Contact } from "../types";
 import { useHistory } from "react-router-dom";
 import { Button } from "antd";
+import delay from "../utils/delay";
+import API from "../service/api";
 
 const Home = observer(() => {
   const history = useHistory();
   const { ContactStore } = useStores();
+
+  const [isDeletingContactId, setIsDeletingContactId] = useState<
+    string | null
+  >();
+
+  ///////////////////////////////////////////
+  // Network
+  ///////////////////////////////////////////
+
+  const deleteContact = async (contactId: string) => {
+    setIsDeletingContactId(contactId);
+
+    await delay(1000);
+
+    try {
+      await API.contact.deleteContact(contactId);
+      ContactStore.removeContact(contactId);
+    } catch (e) {
+      //TODO: Error
+    } finally {
+      setIsDeletingContactId(null);
+    }
+  };
 
   ///////////////////////////////////////////
   // Effects
@@ -28,7 +53,19 @@ const Home = observer(() => {
   // ContactCell Cb
   ///////////////////////////////////////////
   const handleContactCellClick = (contact: Contact) => {
+    if (contact.id === isDeletingContactId) {
+      return;
+    }
+
     history.push(`/contact/${contact.id}`);
+  };
+
+  const handleContactCellDeleteClick = (contact: Contact) => {
+    if (!!isDeletingContactId) {
+      return;
+    }
+    
+    deleteContact(contact.id);
   };
 
   ///////////////////////////////////////////
@@ -44,8 +81,11 @@ const Home = observer(() => {
         {ContactStore.sortedContacts.map((contact: Contact) => (
           <ContactCell
             key={contact.id}
+            isDeleting={contact.id === isDeletingContactId}
+            deleteDisabled={!!isDeletingContactId}
             contact={contact}
             onClick={handleContactCellClick}
+            onDeleteClick={handleContactCellDeleteClick}
           />
         ))}
       </div>
