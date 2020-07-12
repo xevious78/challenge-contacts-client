88d9c95fa5e3519746747b638f3a7306770f5898
Aladin Taleb <aladin.taleb@gmail.com>
2020-07-12 18:31:26 +0200

    [refacto] TypeScript responses in API

--- src/service/api.ts
+++ src/service/api.ts
@@ -1,27 +1,50 @@
 import Request from "./request";
+import { Contact, ContactInfos } from "../types";
+
+interface GetContactsData {
+  contacts: Array<Contact>;
+}
+
+interface CreateContactData {
+  contact: Contact;
+}
+
+interface GetContactData {
+  contact: Contact;
+}
+
+interface UpdateContactData {
+  contact: Contact;
+}
+
+interface DeleteContactData {
+  contact: Contact;
+}
 
 const API = {
   contact: {
     getContacts: async () => {
-      return Request.get("/contacts");
+      return Request.get<GetContactsData>("/contacts");
     },
 
-    createContact: async (contactInfos: any) => {
-      return Request.post("/contacts", { contactInfos: contactInfos });
+    createContact: async (contactInfos: ContactInfos) => {
+      return Request.post<CreateContactData>("/contacts", {
+        contactInfos: contactInfos,
+      });
     },
 
     getContact: async (contactId: string) => {
-      return Request.get(`/contacts/${contactId}`);
+      return Request.get<GetContactData>(`/contacts/${contactId}`);
     },
 
-    updateContact: async (contactId: string, contactInfos: any) => {
-      return Request.put(`/contacts/${contactId}`, {
+    updateContact: async (contactId: string, contactInfos: ContactInfos) => {
+      return Request.put<UpdateContactData>(`/contacts/${contactId}`, {
         contactInfos: contactInfos,
       });
     },
 
     deleteContact: async (contactId: string) => {
-      return Request.delete(`/contacts/${contactId}`);
+      return Request.delete<DeleteContactData>(`/contacts/${contactId}`);
     },
   },
 };
